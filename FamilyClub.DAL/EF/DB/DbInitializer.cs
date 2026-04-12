6ac91f4298c6d1d8478de4e9c037d0f1e8ffa364
Shers_um49 <Shers_um49@student.itstep.org>
2026-04-12 23:47:25 +0300

    Added starting `Admin` and basic `Roles` in DbInitializer class.

--- FamilyClub.DAL/EF/DB/DbInitializer.cs
+++ FamilyClub.DAL/EF/DB/DbInitializer.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace FamilyClub.DAL.EF.DB
 {
 	// Class to initialize database
@@ -24,6 +25,47 @@ namespace FamilyClub.DAL.EF.DB
 				await context.Database.EnsureCreatedAsync();
 			}
 
-		}
+
+            /////////////////////////////////////////////
+            /// ROLES
+            /////////////////////////////////////////////
+            // Getting Identity managers
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ClubMember>>();
+			// Creating basic roles
+			string[] roleNames = { "Admin", "User", "Manager" };
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+			// Creating basic Administrator
+			var adminEmail = "admin@familyclub.com";
+			var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                var admin = new ClubMember
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    Name = "System",
+                    Surname = "Admin",
+                    EmailConfirmed = true, // Important for Identity
+                    PhoneNumber = "0000000000"
+                };
+
+                // Password must correspond to Our requirements
+                var result = await userManager.CreateAsync(admin, "{AdminFamilyClub*777!");
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
 	}
 }
